4a04d36b4e123df61c45065f632ca36f169fc507
Andy Wilkinson <andy@wtribe.com>
2015-05-25 12:31:07 -0700

    Version 1.7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,6 +35,4 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.6.*")]
-
-
+[assembly: AssemblyVersion("1.7.*")]
